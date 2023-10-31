c128ccfed52d80a568c97d302f4d3eb18d7f8aaa
Champ-Wei <hongbo_wei@outlook.com>
2023-10-31 17:05:02 +0400

    Creating Requests and Responses
    How to use the request and response objects in Django?  When modifying the structure of the HTTP response object, you can also return the attributes of the HTTP request object. For example, you can create variables to return the "address", "method", "path info", "scheme", and "user agent".

--- django/demoproject/demoapp/views.py
+++ django/demoproject/demoapp/views.py
@@ -3,4 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. This is the index view of Demoapp.") 
+    return HttpResponse("Hello, world. This is the index view of Demoapp.")
+
+def home(request):
+    path = request.path
+    address = request.META['REMOTE_ADDR']
+    scheme = request.scheme
+    method = request.method
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+
+    response = HttpResponse()
+    response.headers["Age"] = 20
+
+    msg = f"""<br>
+        <br>Path: {path}
+        <br>Scheme: {scheme}
+        <br>Method: {method}
+        <br>User agent: {user_agent}
+        <br>Path info: {path_info}
+        <br>Response header: {response.headers}
+    """
+
+    return HttpResponse(msg, content_type=' text/html', charset='utf-8')
+
