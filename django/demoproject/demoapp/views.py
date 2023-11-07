e6fb769f1c7cb23ff526be801921fb06de9c9921
Champ-Wei <hongbo_wei@outlook.com>
2023-11-07 12:35:42 +0400

    Update views.py

--- django/demoproject/demoapp/views.py
+++ django/demoproject/demoapp/views.py
@@ -47,4 +47,4 @@ def getform(request):
     if request.method == "POST":
         id=request.POST['id']
         name=request.POST['name']
-    return HttpResponse("Name: {} UserID: {}".format(name, id)) 
+    return HttpResponse("Name: {} UserID: {}".format(name, id))
