0fabef225802b0491cf384f97907ef904e7b2f3b
Champ-Wei <hongbo_wei@outlook.com>
2023-11-12 15:42:54 +0400

    Creating models

--- django/demoproject/demoapp/models.py
+++ django/demoproject/demoapp/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " : " + self.cuisine
