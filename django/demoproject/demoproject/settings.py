42f9069ffc925739e3ae79efde1b6e8e20986af4
Champ-Wei <hongbo_wei@outlook.com>
2023-11-12 17:42:02 +0400

    Update settings.py

--- django/demoproject/demoproject/settings.py
+++ django/demoproject/demoproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'demoapp',
+    'demoapp.app.DemoappConfig',
 ]
 
 MIDDLEWARE = [
